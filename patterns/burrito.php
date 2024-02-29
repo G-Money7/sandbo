b85ab4e8ad5d47e15a887f9f34f204eb0c183570
G-Money7 <gavschroeder22@gmail.com>
2024-02-29 11:49:11 -0600

    added burrito pattern for class homework/demo

---
+++ patterns/burrito.php
@@ -0,0 +1,11 @@
+<?php
+/**
+* Title: Burrito pattern
+* Slug: sandbo/burrito-pattern
+* Categories: featured, food
+*
+* More headers can be found at:
+* https://developer.wordpress.org/themes/features/block-patterns/#registering-block-patterns
+*/
+?>
+<!-- wp:block {"ref":12} /-->
