c8d6967b93dc991531969ef7182ce876e59f5976
G-Money7 <gavschroeder22@gmail.com>
2024-03-04 07:38:22 -0600

    new pattern and branch

---
+++ patterns/portfolio.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Title: Portfolio pattern
+ * Slug: sandbo/portfolio-pattern
+ * Categories: featured, food
+ *
+ * More headers can be found at:
+ * https://developer.wordpress.org/themes/features/block-patterns/#registering-block-patterns
+ */
+?>
