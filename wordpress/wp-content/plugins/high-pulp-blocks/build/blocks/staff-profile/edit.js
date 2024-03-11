7ce03d8126dc1c0cda1ceba4279c661a9ec76c18
G-Money7 <gavschroeder22@gmail.com>
2024-03-10 20:11:33 -0500

    changed stuff hopefully npm dont delete my stuff

--- wordpress/wp-content/plugins/high-pulp-blocks/build/blocks/staff-profile/edit.js
+++ wordpress/wp-content/plugins/high-pulp-blocks/build/blocks/staff-profile/edit.js
@@ -1,3 +1,9 @@
+/**
+ * Retrieves the translation of text.
+ *
+ * @see https://developer.wordpress.org/block-editor/reference-guides/packages/packages-i18n/
+ */
+import { __ } from '@wordpress/i18n';
 import { useBlockProps, MediaUpload, MediaUploadCheck, RichText } from '@wordpress/block-editor';
 import { Button } from '@wordpress/components';
 
